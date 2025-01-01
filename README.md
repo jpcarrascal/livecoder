161203bf8b5adb5b4f9365209178b1de19517996
JP <jp@JP-M1-2439.local>
2025-01-01 11:21:46 -0500

    POC in good shape!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # livecoder
 Aspiration: AI-powered hardware controller for live coding
+
+```
+npx http-server -o -c-1 -p 8080 .
+```
--- scripts.js
+++ scripts.js
@@ -4,12 +4,38 @@ setTimeout(() => {
     midi.setInputHandler(3, midiHandler);
 }, 3000);
 
-var cursorState = "codeEdit"; // possible states: codeEdit, functionSelect
-var currentParameters = [];
+const dropdown = document.getElementById('functionDropdown');
+const codeArea = document.getElementById('code');
+const paramsView = document.getElementById('params');
+
+var globalObj = {
+    set cursorState(value) {
+        cursorState = value;
+        console.log("Cursor State -----> " + value);
+    },
+    get cursorState() {
+        return cursorState;
+    },
+    set currentParameters(value) {
+        currentParameters = value;
+        paramsView.innerText = JSON.stringify(value);
+    },
+    get currentParameters() {
+        return currentParameters;
+    },
+    set insert(value) {
+        codeToInsert = value;
+    },
+    get insert() {
+        return codeToInsert;
+    }
+};
+
+globalObj.cursorState = "codeEdit"; // possible states: codeEdit, functionSelect
+globalObj.currentParameters = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Live Coder script loaded');
-    const codeArea = document.getElementById('code');
     codeArea.value = localStorage.getItem('codeContent') || '';
     
     // Focus the textarea and set the cursor position to the beginning
@@ -25,56 +51,84 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
             moveCursor(event.key === 'ArrowLeft' ? 'left' : 'right');
         } else {
-            removeDropdown();
+            hideDropdown();
         }
     });
 
     codeArea.addEventListener('click', () => {
-        cursorState == "codeEdit";
-        removeDropdown();
+        globalObj.cursorState == "codeEdit";
+        globalObj.currentParameters = [];
+        hideDropdown();
+    });
+
+    dropdown.addEventListener('change', () => {
+        codeArea.focus();
+        globalObj.insert = dropdown.options[dropdown.selectedIndex].text;
+        globalObj.currentParameters = hydraFunctions[dropdown.selectedIndex].params;
+        const cursorPos = codeArea.selectionStart;
+        const textBeforeCursor = codeArea.value.substring(0, cursorPos);
+        const textAfterCursor = codeArea.value.substring(cursorPos);
+        const newText = textBeforeCursor + "." + globalObj.insert + textAfterCursor;
+    
+        // Enable undo by using execCommand
+        const start = codeArea.selectionStart;
+        const end = codeArea.selectionEnd;
+        const lastChar = codeArea.value[end - 1];
+        let separator = ".";
+        if (lastChar === "(") separator = "";
+        document.execCommand('insertText', false, separator + globalObj.insert);
+        codeArea.setSelectionRange(start + globalObj.insert.length + 1, end + globalObj.insert.length);
+        hideDropdown();
     });
 });
+
 
 function midiHandler (midiMessage) {
     const data = midiMessage.data;
     const type = MIDI.messageType(data[0]);
     const dropdown = document.getElementById('functionDropdown');
-    const codeArea = document.getElementById('code');
     if(type == 'NOTE_ON') {
         if(data[1] == 98 && data[2] == 127) {
-            if(cursorState == "codeEdit") {
+            if(globalObj.cursorState == "codeEdit") {
+                showDropdown();
                 dropdown.focus();
-                cursorState = "functionList";
-            } else if(cursorState == "functionList") {
+                globalObj.cursorState = "functionList";
+            } else if(globalObj.cursorState == "functionList") {
+                globalObj.cursorState = "parameterEdit";
                 const event = new Event('change');
                 dropdown.dispatchEvent(event);
-                cursorState = "parameterEdit";
-                removeDropdown();
+                hideDropdown();
                 codeArea.focus();
-            } else if(cursorState == "parameterEdit") {
-                cursorState = "codeEdit";
+            } else if(globalObj.cursorState == "parameterEdit") {
+                let vals = []
+                for(let i = 0; i < globalObj.currentParameters.length; i++) {
+                    vals.push(globalObj.currentParameters[i].value);
+                }
+                const valString = vals.join(", ");
+                insertTextAtCursor(codeArea, valString);
+                globalObj.cursorState = "codeEdit";
             }
         }
     } else if(type == 'CC_CHANGE') {
-        if(cursorState == "codeEdit") {
+        if(globalObj.cursorState == "codeEdit") {
             if(data[1] == 8)
                 moveCursor("left");
             else if(data[1] == 9)
                 moveCursor("right");
-        } else if(cursorState == "functionList") {
+        } else if(globalObj.cursorState == "functionList") {
             if(data[1] == 8) {
                 dropdown.selectedIndex = dropdown.selectedIndex - 1;
             } else if(data[1] == 9) {
                 dropdown.selectedIndex = dropdown.selectedIndex + 1;
             }
-        } else if(cursorState == "parameterEdit") {
+        } else if(globalObj.cursorState == "parameterEdit") {
             // pot CCs: 4, 3, 5, 6
             const pots = [4, 3, 5, 6, 666];
             for(let i = 0; i < pots.length; i++) {
                 if(data[1] == pots[i]) {
-                    if(currentParameters[i] !== undefined && currentParameters[i].type != "string") {
-                        currentParameters[i].value = map2cc(data[2], currentParameters[i].min, currentParameters[i].max);
-                        console.log(currentParameters);
+                    if(globalObj.currentParameters[i] !== undefined && globalObj.currentParameters[i].type != "string") {
+                        globalObj.currentParameters[i].value = map2cc(data[2], globalObj.currentParameters[i].min, globalObj.currentParameters[i].max);
+                        globalObj.currentParameters = [...globalObj.currentParameters]; // Trigger setter
                     }
                 }
             }
@@ -84,39 +138,31 @@ function midiHandler (midiMessage) {
 
 
 function moveCursor(direction) {
-    const codeArea = document.getElementById('code');
-    const value = codeArea.value;
+    const text = codeArea.value;
     const cursorPos = codeArea.selectionStart;
     const specialChars = [')', '('];
     let newPos = cursorPos;
 
     if (direction === 'left') {
         for (let i = cursorPos - 2; i >= 0; i--) {
-            if (specialChars.includes(value[i])) {
+            if (specialChars.includes(text[i])) {
                 newPos = i + 1;
                 break;
             }
         }
     } else if (direction === 'right') {
-        for (let i = cursorPos + 1; i < value.length; i++) {
-            if (specialChars.includes(value[i])) {
+        for (let i = cursorPos; i < text.length; i++) {
+            if (specialChars.includes(text[i])) {
                 newPos = i + 1;
                 break;
             }
         }
     }
-
     codeArea.setSelectionRange(newPos, newPos);
-    showDropdown();
 }
 
 function showDropdown() {
-    removeDropdown(); // Ensure any existing dropdown is removed
-
-    const codeArea = document.getElementById('code');
-    const dropdown = document.createElement('select');
-    dropdown.id = 'functionDropdown';
-    dropdown.size = 5;
+    dropdown.innerHTML = ''; // Clear existing options
 
     for (let i = 0; i < hydraFunctions.length; i++) {
         const func = hydraFunctions[i];
@@ -126,28 +172,6 @@ function showDropdown() {
         dropdown.appendChild(option);
     }
 
-    dropdown.addEventListener('change', () => {
-        codeArea.focus();
-        const selectedFunction = dropdown.options[dropdown.selectedIndex].text;
-        currentParameters = paramParser(hydraFunctions[dropdown.selectedIndex].params);
-        console.log(currentParameters);
-        const cursorPos = codeArea.selectionStart;
-        const textBeforeCursor = codeArea.value.substring(0, cursorPos);
-        const textAfterCursor = codeArea.value.substring(cursorPos);
-        const newText = textBeforeCursor + "." + selectedFunction + textAfterCursor;
-
-        // Enable undo by using execCommand
-        const start = codeArea.selectionStart;
-        const end = codeArea.selectionEnd;
-        const lastChar = codeArea.value[end - 1];
-        let separator = ".";
-        if (lastChar === "(") separator = "";
-        document.execCommand('insertText', false, separator + selectedFunction);
-        codeArea.setSelectionRange(start + selectedFunction.length + 1, end + selectedFunction.length);
-
-        //removeDropdown();
-    });
-
     const rect = codeArea.getBoundingClientRect();
     const top = rect.bottom + window.scrollY; // Position below the textarea
     const left = rect.left + window.scrollX;
@@ -155,8 +179,14 @@ function showDropdown() {
     dropdown.style.position = 'absolute';
     dropdown.style.left = `${left}px`;
     dropdown.style.top = `${top}px`;
+    dropdown.style.display = 'block'; // Show the dropdown
+}
 
-    document.body.appendChild(dropdown);
+function hideDropdown() {
+    const dropdown = document.getElementById('functionDropdown');
+    if (dropdown) {
+        dropdown.style.display = 'none'; // Hide the dropdown
+    }
 }
 
 function removeDropdown() {
@@ -206,6 +236,19 @@ function determineType(value) {
 // maps a value from range 0-127 to a new range:
 // https://stackoverflow.com/questions/5731863/mapping-a-numeric-range-onto-another
 function map2cc(value, low, high) {
-    return low + (high - low) * (value) / 127;
+    const result = low + (high - low) * (value) / 127;
+    return Math.round((result + Number.EPSILON) * 100) / 100
 }
 
+function insertTextAtCursor(textArea, text) {
+    const startPos = textArea.selectionStart;
+    const endPos = textArea.selectionEnd;
+    const textBeforeCursor = textArea.value.substring(0, startPos);
+    const textAfterCursor = textArea.value.substring(endPos);
+
+    textArea.value = textBeforeCursor + text + textAfterCursor;
+
+    // Update cursor position
+    const newCursorPos = startPos + text.length;
+    textArea.setSelectionRange(newCursorPos, newCursorPos);
+}
